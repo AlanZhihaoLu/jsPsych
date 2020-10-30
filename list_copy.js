18da1827a7ffd4ca79e3fd46a9b163cab6c90290
AlanZhihaoLu <45406389+AlanZhihaoLu@users.noreply.github.com>
2020-10-30 13:14:38 -0400

    Add files via upload

---
+++ list_copy.js
@@ -0,0 +1,26 @@
+var test_stimuli = [
+  {
+    "SceneImage": "basketball_net.jpg",
+    "Object": "Ball",
+    "Inverted": "Yes",
+    "Consistent": "Yes"
+  },
+  {
+    "SceneImage": "bell.jpg",
+    "Object": "Printer",
+    "Inverted": "Yes",
+    "Consistent": "No"
+  },
+  {
+    "SceneImage": "bin.jpg",
+    "Object": "Ottoman",
+    "Inverted": "No",
+    "Consistent": "Yes"
+  },
+  {
+    "SceneImage": "blender.jpg",
+    "Object": "Tree",
+    "Inverted": "No",
+    "Consistent": "No"
+  }
+]
